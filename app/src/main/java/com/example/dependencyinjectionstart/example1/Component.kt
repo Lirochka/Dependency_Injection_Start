53fdd6ecb79a52494ee18e7fe17a6408a5b062bc
Lira <lir0na@mail.ru>
2023-07-21 19:38:05 +0300

    Created Class Component

---
+++ app/src/main/java/com/example/dependencyinjectionstart/example1/Component.kt
@@ -0,0 +1,21 @@
+package com.example.dependencyinjectionstart.example1
+
+class Component {
+
+    fun getComponent(): Computer {
+        val monitor = Monitor()
+        val keyboard = Keyboard()
+        val mouse = Mouse()
+        val computerTower = ComputerTower(
+            Storage(),
+            Memory(),
+            Processor()
+        )
+        return Computer(monitor, computerTower, keyboard, mouse)
+    }
+
+    fun inject(activity: Activity){
+        activity.computer = getComponent()
+        activity.keyboard = Keyboard()
+    }
+}
