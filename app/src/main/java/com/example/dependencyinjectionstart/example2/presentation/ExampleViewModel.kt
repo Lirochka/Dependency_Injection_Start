396bbab8e3a7117ede3efc6e44197d111feb2c9f
Lira <lir0na@mail.ru>
2023-07-22 17:20:36 +0300

    Created Factory for ViewModel

--- app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/ExampleViewModel.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/ExampleViewModel.kt
@@ -1,12 +1,13 @@
 package com.example.dependencyinjectionstart.example2.presentation
 
 import android.util.Log
+import androidx.lifecycle.ViewModel
 import com.example.dependencyinjectionstart.example2.domain.ExampleUseCase
 import javax.inject.Inject
 
 class ExampleViewModel @Inject constructor(
     private val useCase: ExampleUseCase
-) {
+): ViewModel() {
 
     fun method() {
         useCase()
--- app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/MainActivity.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.dependencyinjectionstart.example2.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.lifecycle.ViewModelProvider
 import com.example.dependencyinjectionstart.R
 import com.example.dependencyinjectionstart.example2.di.App
 import com.example.dependencyinjectionstart.example2.di.DaggerApplicationComponent
@@ -11,7 +12,12 @@ import javax.inject.Inject
 class MainActivity : AppCompatActivity() {
 
     @Inject
-    lateinit var viewModel: ExampleViewModel
+    lateinit var viewModelFactory: ViewModelFactory
+
+
+    private val viewModel by lazy {
+        ViewModelProvider(this, viewModelFactory)[ExampleViewModel::class.java]
+    }
 
     private val component by lazy {
         (application as App).component
